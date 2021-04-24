edcb5c81b91c246be31a6862015147b11d71f884
fighteros <am5419@fayoum.edu.eg>
2021-04-24 07:53:18 +0200

    created one section

--- assest/JS/solid.js
+++ assest/JS/solid.js
@@ -1,5 +1,18 @@
 !(function($) {
     "use strict";
+    // typed
+    if ($('.typed').length) {
+        var typed_strings = $(".typed").data('typed-items');
+        typed_strings = typed_strings.split(',')
+        new Typed('.typed', {
+            strings: typed_strings,
+            loop: true,
+            typeSpeed: 100,
+            backSpeed: 50,
+            backDelay: 2000
+        });
+    }
+
     // nav colored
     var navbar = document.getElementById('navbar');
     $(window).scroll(function() {
