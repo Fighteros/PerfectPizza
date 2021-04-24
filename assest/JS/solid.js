4c6b72c234b1906c0b500fe1a0f7a0cafde0196c
fighteros <am5419@fayoum.edu.eg>
2021-04-24 05:03:06 +0200

    finished navbar mob and web responsive

--- assest/JS/solid.js
+++ assest/JS/solid.js
@@ -1,4 +1,18 @@
 !(function($) {
+    "use strict";
+    // nav colored
+    var navbar = document.getElementById('navbar');
+    $(window).scroll(function() {
+        var scrollTop = $(window).scrollTop();
+        if (scrollTop > $('#header').offset().top) {
+            $(navbar).addClass('nav-colored');
+            $(navbar).reomveClass('nav-transparent')
+        } else {
+            $(navbar).removeClass('nav-colored');
+            $(navbar).addClass('nav-transparent')
+        }
+    });
+
     // toggle button icon for mobile navbar
     var navBtn = document.getElementsByClassName('navbar-toggler');
     var iIcon = document.getElementById("navicon");
