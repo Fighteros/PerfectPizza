0f1ec9e9a557108d0d3c9e3a0fb065af43dbe0f8
fighteros <am5419@fayoum.edu.eg>
2021-04-25 00:02:05 +0200

    stop animations on small devices

--- assest/JS/main.js
+++ assest/JS/main.js
@@ -47,4 +47,13 @@
     });
 
 
+    // stop animations on small devices
+
+    if (isMobile || isTablet) {
+        $('.wow').addClass('wow-removed').removeClass('wow');
+    } else {
+        $('.wow-removed').addClass('wow').removeClass('wow-removed');
+    }
+
+
 })(jQuery)
